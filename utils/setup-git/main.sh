99e514f0433cd672b88e5cb2e6222a6d658a2db0
Dwi Siswanto <me@dw1.io>
2024-03-20 13:42:23 +0700

    refactor(utils): runs with bash scripts for setup-git
    Signed-off-by: Dwi Siswanto <me@dw1.io>

---
+++ utils/setup-git/main.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+set -euo pipefail
+
+echo "::debug::Prepend user's ID to user's email"
+if [[ "${USERNAME}" == "${GITHUB_ACTOR}" ]]; then
+	EMAIL="${GITHUB_ACTOR_ID}+${EMAIL}"
+fi
+
+echo "::debug::Set git user's email"
+git config --$SCOPE user.email "${EMAIL}"
+
+echo "::debug::Set git user's name"
+git config --$SCOPE user.name "${USERNAME}"
+
+echo "email=${EMAIL}" >> $GITHUB_OUTPUT
