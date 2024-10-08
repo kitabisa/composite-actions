3b6b318fac8853dc6803a44e90773e55dcf33e8e
dimzrio <driotantowi@gmail.com>
2024-10-08 11:17:08 +0700

    🧹 chore: │  enable better-commit and pre-commit check
    Changelog: chore

---
+++ branch-convention.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+REGEX="^(feat|fix|docs|refactor|perf|test|build|ci|chore)\/[A-Za-z0-9._-]+$"
+
+if [[ -z "${BRANCH}" ]]; then
+    BRANCH=$(git rev-parse --abbrev-ref HEAD)
+else
+    BRANCH="${BRANCH}"
+fi
+
+echo $BRANCH
+
+if ! [[ $BRANCH =~ $REGEX ]]; then
+  echo "Your commit was rejected due to branching name"
+  echo "Please rename your branch with '(feat|fix|docs|refactor|perf|test|build|ci|chore)/lowercase-name' syntax"
+  echo "Or you can use better-branch tools"
+  exit 1
+fi
